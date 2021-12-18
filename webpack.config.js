131ed95f3e690e1adfbb23d1ecc0e7593c61cb20
ivan0313 <ivan0313@connect.hku.hk>
2021-12-18 14:24:22 +0000

    fix: yarn prod command

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'assets'),
     },
 
-    devtool: production ? '' : 'source-map',
+    devtool: production ? false : 'source-map',
   
     resolve: {
       extensions: [".js", ".jsx", ".json"],
